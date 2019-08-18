efdd7eebd332f30c8bbfa1d4eda7064ee5614cdf
Alexandre Okita <37855969+AlexandreOkita@users.noreply.github.com>
2019-08-18 04:21:53 -0300

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -6,16 +6,19 @@ from tkinter import PhotoImage
 import recog
 import ObjectDetector
 
+process_frame = 0
+cont = 0
 root = tk.Tk()
-w = tk.Frame(root,height="400", width="900", bg="black")
+w = tk.Frame(root,height="400", width="1280", bg="black")
 w.pack()
 w.pack_propagate(0) 
 foto = ImageTk.PhotoImage(Image.open("dados.jpg"))
+pop = ImageTk.PhotoImage(Image.open("popup.jpg"))
 lmain = tk.Label(w)
 lmain.pack()
 cap = recog.ligar_camera()
 lista = recog.guardar_participantes('okita.jpg', 'caio.jpg')
-
+od = ObjectDetector.Object_Detector()
 
 def popup():
     toplevel = tk.Toplevel()
@@ -23,9 +26,10 @@ def popup():
     label1.pack()
     label2 = tk.Label(toplevel, height=0, width=100)
     label2.pack()
+    label3 = tk.Label(toplevel, image=pop)
+    label3.pack()
 
 def show_frame(frame):
-        print('teste')
         img = Image.fromarray(frame)
         imgtk = ImageTk.PhotoImage(image=img)
         lmain.imgtk = imgtk
@@ -33,15 +37,34 @@ def show_frame(frame):
         root.update()
 
 def prin():
-        cont = 0
-        rgb = recog.capturar_frame(cap)
-        coord = recog.reconhecer_rosto(lista, rgb[0])
-        frame = recog.desenhar(coord[0], coord[1], rgb[1])
+        global process_frame
+        global cont
+        ret, frame = recog.capturar_frame(cap)
+        coord = recog.reconhecer_rosto(lista, ret)
+        if process_frame%5 == 0:
+            frame = od.process(frame)
+            process_frame = 0
+        else:
+            frame = recog.desenhar(coord[0], coord[1], frame)            
+            
+        process_frame += 1
+        print('NAMES:',coord[1])
+        print('CONT:',cont)
+        for i in coord[1]:
+                if i == "Unknown":
+                        cont += 1
+                        if (cont == 10):
+                                popup()
+                                cont = -1
+                                
+                elif cont != -1:
+                        cont = 0
+                
         show_frame(frame)
 
 def main(root):
-    dados = tk.Label(root, image=foto, bg="#E6E8FA", height="343")
-    dados.pack(fil=tk.Y)
+    dados = tk.Label(root, image=foto, bg="#E6E8FA", height="233", width="1280")
+    dados.pack(fill=tk.Y)
     dados.pack_propagate(0)
     while 1:
         prin()
@@ -64,7 +87,7 @@ def main(root):
  #   root = tk.Tk()
   #  Example(root).pack(fill="both", expand=True)
    # root.mainloop()
-root.geometry("900x638")
+root.geometry("1280x633")
 main(root)
 root.mainloop()
 
